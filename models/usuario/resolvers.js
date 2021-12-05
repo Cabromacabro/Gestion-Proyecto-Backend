9a7b8df859a8b9d8e97bb7dd2ebb50bf61ccf8c4
Cabromacabro <oscarpedraza1@hotmail.com>
2021-12-05 16:44:51 -0500

    Mejorar Consulta en resolvers usuario

--- models/usuario/resolvers.js
+++ models/usuario/resolvers.js
@@ -2,20 +2,26 @@ import { UserModel } from './usuario.js';
 import bcrypt from 'bcrypt';
 
 const resolversUsuario = {
+  Usuario: {
+    inscripciones: async (parent, args, context) => { 
+      return InscripcionModel.find({ estudiante: parent._id });
+    },
+  },
   Query: {
     Usuarios: async (parent, args, context) => {
-      const usuarios = await UserModel.find().populate([
-        {
-          path: 'inscripciones',
-          populate: {
-            path: 'proyecto',
-            populate: [{ path: 'lider' }, { path: 'avances' }],
-          },
-        },
-        {
-          path: 'proyectosLiderados',
-        },
-      ]);
+      const usuarios = await UserModel.find();
+      // .populate([
+      //   {
+      //     path: 'inscripciones',
+      //     populate: {
+      //       path: 'proyecto',
+      //       populate: [{ path: 'lider' }, { path: 'avances' }],
+      //     },
+      //   },
+      //   {
+      //     path: 'proyectosLiderados',
+      //   },
+      // ]);
       return usuarios;
     },
     Usuario: async (parent, args) => {
